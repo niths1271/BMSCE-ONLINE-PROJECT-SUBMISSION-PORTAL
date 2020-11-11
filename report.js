109a5519e5cbd243b59a1e3ccf06a9726055733a
NITHIN.C <71306445+niths1271@users.noreply.github.com>
2020-11-11 20:36:30 +0530

    clear all
    clear all pages

--- report.js
+++
@@ -1,9 +0,0 @@
-function makechg() {
-    var a = document.getElementsByClassName('disap')[0];
-    var b = document.getElementsByClassName('disap')[1];
-    var c = document.getElementsByClassName('table')[0];
-alert("YOUR REPORT HAS BEEN SUBMITTED SUCCESFULLY");
-    a.style.display = 'none';
-    b.style.display = 'none';
-    c.style.display = 'block';
-}
