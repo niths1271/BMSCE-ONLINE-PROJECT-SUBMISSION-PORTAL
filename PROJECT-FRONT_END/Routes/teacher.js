f90e167335f50c2178b0d00a6c2fb37ec226f272
niranjansavanur <niranjansavanur26@gmail.com>
2021-06-03 23:15:22 +0530

    aaaaa

--- PROJECT-FRONT_END/Routes/teacher.js
+++ PROJECT-FRONT_END/Routes/teacher.js
@@ -10,9 +10,15 @@ router.get("/viewreport",function(req,res){
     if(err){
       console.log(err);
     }else{
-      console.log(result1);
+      //console.log(result1);
       var namesObj=[];
       var docsObj=[];
+<<<<<<< Updated upstream
+=======
+      function getFinal(){
+        return new Promise(function(resolve,reject){
+           result1.forEach((result, index)=>{
+>>>>>>> Stashed changes
         function names(){
           return new Promise(function(resolve,reject){
             result1.forEach((result)=>{
@@ -21,8 +27,13 @@ router.get("/viewreport",function(req,res){
               if(err){
                 console.log(err);
               }else{   
+<<<<<<< Updated upstream
                 console.log(result2);
                 namesObj.push(result2);
+=======
+                //console.log(result2);
+                resolve(result2);
+>>>>>>> Stashed changes
               }
             });
           });
@@ -37,8 +48,13 @@ router.get("/viewreport",function(req,res){
             if(err){
               console.log(err);
             }else{   
+<<<<<<< Updated upstream
               console.log(result2[0]);
               docsObj.push(result2[0]);
+=======
+              //console.log(result2[0]);
+              resolve(result2[0]);
+>>>>>>> Stashed changes
             }
           });
         });
@@ -51,13 +67,18 @@ router.get("/viewreport",function(req,res){
       console.log(docsObj); 
 =======
     names().then((name)=>{
-      namesObj.push(name);
-      console.log("aa",namesObj);
+      //namesObj.push(name);
+      docs().then((doc)=>{
+      docsObj.push([...name, doc, {TITLE:result1.PROJECT_TITLE}]);
+      //console.log("z",docsObj); 
+      console.log("holaaaa",index, " ", result1.length );
+      if(index==result1.length-1){
+        console.log("loop done");
+        resolve();
+      }
     });
-    docs().then((doc)=>{
-      docsObj.push(doc);
-      console.log("bb",docsObj);
     });
+    
       // namesObj.push(await names());
       // docsObj.push(await docs());
        
@@ -65,10 +86,24 @@ router.get("/viewreport",function(req,res){
     
     
       });
+<<<<<<< Updated upstream
       console.log("z",namesObj);
       console.log("z",docsObj); 
 >>>>>>> 526e03d4befa608b5bccb6d23e7bffdf26c73129
       res.render("treport",{tname:result1[0].NAME,temail:result1[0].EMAIL});
+=======
+          
+        });
+      }
+      getFinal().then(()=>{
+        console.log("got", docsObj);
+        res.render("treport",{tname:result1[0].NAME,temail:result1[0].EMAIL, values:docsObj});
+      });
+     
+     // console.log("z",namesObj);
+      
+      
+>>>>>>> Stashed changes
     }
   });
   }else{
