bb5b0fa32ab16b97a59a68befb5f94a1136ddbf2
niranjansavanur <niranjansavanur26@gmail.com>
2021-06-21 00:42:46 +0530

    aa

--- PROJECT-FRONT_END/Routes/admin.js
+++ PROJECT-FRONT_END/Routes/admin.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const insertMembers = require('../controllers/insertmembers');
 var uuid = require('uuid');
 const { report } = require(".");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 router.get("/admindashboard", function(req, res) {
     if (req.isAuthenticated() && req.user.ROLE==="ADMIN") {
@@ -21,6 +23,41 @@ router.get("/addTeacher", function(req, res){
     }else{
         res.redirect("/adminuser/login");
     }
+});
+
+router.post("/addTeacher", function(req, res){
+    console.log("mamu",req.body);
+    var details = req.body;
+    var pw = details.password;
+    bcrypt.hash(pw, saltRounds, function(error, hash){
+        if(error){
+            console.log(error);
+        }else{
+            connection.query(`INSERT INTO USER_DETAILS SET ?`, {USERNAME:details.username,PASSWORD:hash, ROLE:details.role}, err=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log("1st insert success!");
+            connection.query(`SELECT id FROM USER_DETAILS WHERE USERNAME='${details.username}';`, function(er, rs){
+                if(er){
+                    console.log(er);
+                }else{
+                    console.log(rs[0]);
+                    connection.query(`INSERT INTO TEACHER_DETAILS SET ?`, {USER_ID:rs[0].id,EMAIL:details.email,PHONE_NO:details.phone, NAME:details.name}, eror=>{
+                        if(eror){
+                            console.log(eror);
+                        }else{
+                            console.log("2nd insert success!");
+                            res.redirect("/admin/addTeacher");
+                        }
+                    });
+                }
+            });
+        }
+    });
+        }
+    });
+    
 });
 
 module.exports = router;
